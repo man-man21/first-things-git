1ccf34199515dce5532a0198597e106b576699e3
man-man21 <wiktor.st99@gmail.com>
2022-10-22 11:12:09 +0200

    Finished small project

---
+++ tablica z wynikiem/index.js
@@ -0,0 +1,48 @@
+let scoore = document.getElementById("score1")
+let score = 0
+
+function one1() {
+    score += 1
+    scoore.textContent = score
+}
+
+function two1() {
+    score += 2
+    scoore.textContent = score
+}
+
+function three1() {
+    score += 3
+    scoore.textContent = score
+}
+
+function reset1() {
+    score = 0
+    scoore.textContent = score
+}
+
+///////////////////////////////////////////////////////////////////////////
+let scure = document.getElementById("score2")
+let score2 = 0
+
+function one2() {
+    score2 += 1
+    scure.textContent = score2
+}
+
+function two2() {
+    score2 += 2 
+    scure.textContent = score2
+}
+
+function three2() {
+    score2 += 3
+    scure.textContent = score2
+}
+
+
+function reset2() {
+    score2 = 0
+    scure.textContent = score2
+}
+
